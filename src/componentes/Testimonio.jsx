8dab8483d32b5a4bf8da8169246fde3210b59e6b
kevin.argumedo <kevin.argumedo@gmail.com>
2023-04-07 18:24:18 -0500

    public img

--- src/componentes/Testimonio.jsx
+++ src/componentes/Testimonio.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Testimonio(props) {
-  const imagenSrc = `src/image/testimonio-${props.imagen}.png`;
+  const imagenSrc = `./image/testimonio-${props.imagen}.png`;
   return (
     <div className="w-1170 h-368 bg-white flex items-center justify-center shadow-Shadow text-black my-4 ">
       <img className="w-auto h-full" src={imagenSrc} alt="foto-testigo" />
