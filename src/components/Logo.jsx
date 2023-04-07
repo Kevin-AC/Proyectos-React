2fa8976ecd5ce7e24d3f9cc1555ddf15752f0cc2
kevin.argumedo <kevin.argumedo@gmail.com>
2023-04-07 18:27:02 -0500

    public img

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ function Logo(){
         <div className="w-full m-5 flex items-center justify-center ">
         <img
           className="w-[400px] h-[50px] "
-          src="src/image/freecodecamp-logo.png"
+          src="/image/freecodecamp-logo.png"
           alt="frecodecamp-logo"
         />
       </div>
