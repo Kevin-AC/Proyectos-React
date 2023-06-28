ef53d16194a0718003ae3c94e9ee4eec2025f203
Kevin-AC <kevin.argumedo@gmail.com>
2023-06-28 16:26:22 -0500

    add: darkmode card

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        azul: "#03d3",
+      },
+    },
+  },
+  plugins: [],
+};
